b2a5c96e2ad59a0c2577e72f6fe9ac39d7a426b6
Bas Baccarne <bastiaan.baccarne@ugent.be>
2022-09-01 18:09:22 +0200

    added 2way interaction

--- protopie/SimpleButton.ino
+++ protopie/SimpleButton.ino
@@ -1,5 +1,3 @@
-// this codeblock reads the state of a button and sends changes as a 1/0 serial message over USB
-
 // wiring:
 // 3.3V <--> button side 1
 // button side 2 <--> digital pin 2
@@ -7,6 +5,7 @@
 
 // variables
 int oldButtonState = LOW;
+char oldScreenbuttonState = 'o';
 
 // pin numbers
 int buttonPin = 2;
@@ -15,19 +14,33 @@ int buttonPin = 2;
 void setup() {
   Serial.begin(9600);
   pinMode(buttonPin, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
   
 }
 
 // loop
 void loop() {
-  // read button
+  // ARDUINO 2 PROTOPIE - read button state and send changes through a serial message
   int newButtonState = digitalRead(buttonPin);
-
-  // check for changes
+  
   if(newButtonState != oldButtonState){
      Serial.println(newButtonState);
      oldButtonState = newButtonState;
   }
-  
+
+  // PROTOPIE 2 ARUINO - check for led message and turn internal led on if detected
+  char newScreenbuttonState = Serial.read();
+
+  if(newScreenbuttonState == 'i'){
+    digitalWrite(LED_BUILTIN, HIGH);
+    oldScreenbuttonState = newScreenbuttonState;
+  }
+
+    if(newScreenbuttonState == 'o'){
+    digitalWrite(LED_BUILTIN, LOW);
+    oldScreenbuttonState = newScreenbuttonState;
+  }
+
+  // delay
   delay(1); 
 }
