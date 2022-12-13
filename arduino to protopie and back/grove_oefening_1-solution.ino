47e7c4ca382f38fe5caba61551001af8bec68ee4
Bas Baccarne <bastiaan.baccarne@ugent.be>
2022-12-13 15:31:34 +0100

    change button to rotary angle sensor

--- arduino to protopie and back/grove_oefening_1-solution.ino
+++ arduino to protopie and back/grove_oefening_1-solution.ino
@@ -3,42 +3,45 @@
   This script shows how to interact between Protopie and Arduino using Serial Communication
   The hardware entails:
     * A Grove-Red LED connected to port D2 of a Grove-Base Shield
-    * A Grove Grove-Button to port D4 of a Grove-Base Shield
+    * A Grove Grove-Rotary Angle Sensor(P) to port A0 of a Grove-Base Shield
 
-  modified December 10th 2022
+  modified December 13 2022
   by Bas Baccarne
 */
 
 // define variables
-int oldButtonState = LOW;
+int oldSensorValue;
 String oldScreenbuttonState = "led_off";
 String newScreenbuttonState ="led_off";
 
-// define pin numbers of led (D2) and button (D4)
-const int ledPin = 2;
-const int buttonPin = 4;
+// define pin numbers of led (D2) and rotary angle sensor (A0)
+#define ledPin 2
+#define rotarySensor A0
 
 void setup() {
   // open serial communication
   Serial.begin(9600);
 
   // define input and output
-  pinMode(buttonPin, INPUT);
+  pinMode(rotarySensor, INPUT);
   pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  // DIRECTION 1: ARDUINO TO PROTOPIE (send a message when the button is pressed)
+  // DIRECTION 1: ARDUINO TO PROTOPIE (send the value of the rotary angle sensor)
   
-  // read button state
-  int newButtonState = digitalRead(buttonPin);
+  // read rotary sensor state
+  int sensorValue = analogRead(rotarySensor);
 
-  // when the button is pressed (1) OR released (0), send the value over serial using the variable "button"
-  if (newButtonState != oldButtonState) {
-      Serial.print("button||");
-      Serial.println(newButtonState);
-      oldButtonState = newButtonState;
-    }
+  // if the rotaty angle sensor changes, send the new value
+  // we only send data when something changes to be as sparse as possible on the transactions
+  if (oldSensorValue != sensorValue){
+    // reset the change detection variable
+    oldSensorValue = sensorValue;
+    // send the angle value (0 to 1023 as variable "angle")
+    Serial.print("angle||");
+    Serial.println(sensorValue);
+  } 
 
   // DIRECTION 2: PROTOPIE 2 ARDUINO (check for "led" message and turn led on if detected)
 
